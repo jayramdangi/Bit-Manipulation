db338b1e0821f84300d115162657a3686531ac33
jayramdangi <128122311+jayramdangi@users.noreply.github.com>
2024-07-19 14:24:51 +0530

    Update bit3.cpp

--- bit3.cpp
+++ bit3.cpp
@@ -12,7 +12,9 @@ int main (){
      b = b^a; // b^a = b^a^b = a^0 = a 
      a = a^b; // a^b = a^b^a = b^0 = b
      cout<<a<<" "<<b<<endl;
-     // now  we are given an array and one element has the odd  count find that element 
+     // now  we are given an array and one element has the odd  count find that element
+     // this complex problem can be solved with the help of the XOR operation though it can be solved through map but it will take more space compare
+     // to the XOR operation so we know a^a =0 so all the element with the even count will become zero and one's with odd count will remain 
      int arr[13]={2,3,9,7,4,8,8,2,3,4,9,4,7};
      int ans =0; 
      for(int i =0 ; i<13; i++)
